130f7a95d705c7182c9004312e8e655d5cef1c93
Billy Ott <willrichott@gmail.com>
2020-07-18 04:34:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,6 +15,7 @@ WIN_COMBINATIONS = [
     [2,4,6]
   ]
 
+
 def won?(board)
   output = false
   WIN_COMBINATIONS.each do |win_combination|
@@ -68,6 +69,7 @@ def over?(board)
   end
   return output
 end
+
 
 def winner?(board)
   if won?(board) != false && 
